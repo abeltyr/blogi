fe326fcea3bfaed5f0b905c92ad74211fa6b6136
avell21 <abeltyr@gmail.com>
2018-09-21 15:38:47 +0300

    follower

---
+++ migrations/20180906110400-create-follow-user.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable("follow_users", {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      follower_id: {
+        type: Sequelize.UUID,
+        allowNull: false
+      },
+      followed_id: {
+        type: Sequelize.UUID,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }),
+  //eslint-disable-line
+  down: (queryInterface, Sequelize) => queryInterface.dropTable("follow_users")
+};
---
+++ migrations/20180906110528-create-follow-category.js
@@ -0,0 +1,33 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("follow_categories", {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("follow_categories");
+  }
+};
