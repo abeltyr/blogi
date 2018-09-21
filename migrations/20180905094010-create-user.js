c848e1245740e8eb28dd95accded00ba1ae128a3
MikeAbebe <mikiastilahun@gmail.com>
2018-09-21 15:05:35 +0300

    configured google loggin

---
+++ migrations/20180905094010-create-user.js
@@ -0,0 +1,45 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      google_id: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      facebook_id: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      full_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("users");
+  }
+};
