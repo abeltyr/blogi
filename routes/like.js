5a13082c25218235b8435d26361aac373d784dd8
avell21 <abeltyr@gmail.com>
2018-09-24 07:38:04 +0300

    like & blog modifed

--- routes/like.js
+++ routes/like.js
@@ -9,10 +9,13 @@ const token = require("../app/middlewares/verifyToken");
 app
   // apply the auth middleware
   .use((req, res, next) => token(req, res, next))
+
   // url for liking a blog
   .get("/like/:blog_id", like.like)
+
   // url for unlike
-  // .get("/unlike/:blog_id", like.unlike)
+  .get("/unlike/:blog_id", like.unlike)
+
   // url for getting a like count for a specific blog
   .get("/likes/:blog_id", like.getLikeCount);
 
