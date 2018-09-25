e03dcc40c87bd7a8a117c85a3e11fd3b474a6bd5
MikeAbebe <mikiastilahun@gmail.com>
2018-09-25 13:10:35 +0300

    test deploymet

--- app/middlewares/verifyToken.js
+++ app/middlewares/verifyToken.js
@@ -1,19 +1,19 @@
 const jwt = require("jsonwebtoken");
-const time = require("moment");
+//const time = require("moment");
 
 function verifyToken(req, res, next) {
   if (req.headers.token) {
     jwt.verify(req.headers.token, process.env.SECRET, (err, doc) => {
       if (err) {
-        res.status(401).send("invalid token provided");
+        res.status(401).json("invalid token provided");
       }
       // todo add the expire date for the token to verify
-      if (time(doc.expired_date) < time().now()) {
-        res.status(401).send("the token has expired");
-      }
+      // if (time(doc.expired_date) < time().now()) {
+      //   res.status(401).json("the token has expired");
+      // }
       req.user = doc;
       next();
     });
-  } else res.status(401).send("token not provided");
+  } else res.status(401).json("token not provided");
 }
 module.exports = verifyToken;
