a8633c18f11472e2a09d3b6d3b7669b95a8cd49b
avell21 <abeltyr@gmail.com>
2018-09-26 13:13:22 +0300

    db table have been changed migrate

--- routes/user.js
+++ routes/user.js
@@ -22,15 +22,8 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "https://ethblogi1.herokuapp.com/user/facebook/callback",
-      profileFields: [
-        "id",
-        "gender",
-        "email",
-        "picture.type(small)",
-        "displayName",
-        "link"
-      ]
+      callbackURL: "http://localhost:3000/user/facebook/callback",
+      profileFields: ["id", "displayName", "photos", "link", "email"]
     },
     (accessToken, refreshToken, profile, cb) => {
       cb(null, profile);
@@ -58,56 +51,58 @@ app.get(
 );
 
 // login link /user/facebook/login
-app.get(
-  "/facebook/login",
-  passport.authenticate(
-    "facebook",
-    {
-      session: false
-    },
-    {
-      scope: ["email"]
-    }
-  )
-);
+app.get("/facebook/login", passport.authenticate("facebook"));
 
 // facebook callback
 app.get("/facebook/callback", (req, res, next) => {
   // eslint-disable-next-line consistent-return
   passport.authenticate(
     "facebook",
-    { scope: ["email", "public_profile"] },
+    {
+      scope: ["email", "public_profile"],
+      authType: "rerequest"
+    },
+    // eslint-disable-next-line
     (err, user) => {
       if (err) return next(err);
       if (!user) return res.redirect("/user/login");
-      // db.user.findOrCreate({
-      //         where: {
-      //             facebook_id: user.id,
-      //             full_name: user.displayName,
-      //             image: "null",
-      //             email: "null"
-      //         }
-      //     }).then(doc => {
-      //         db.user.findOne({
-      //             where: {
-      //                 id: doc.id
-      //             }
-      //         }, {
-      //             image: user.photos[0].value
-      //         }).then(moddoc => {
-      //             let body = {
-      //                 facebook_id: user.id,
-      //                 full_name: user.displayName,
-      //                 image: user.photos[0].value,
-      //                 email: "null"
-      //             }
-      //             return res.json({
-      //                 "access_token": jwt.sign(body, process.env.SECRET),
-      //                 "type": "Bearer"
-      //             })
-      //         })
-      //     })
-      //     .catch(err => res.status(500).json(err))
+      db.user
+        .findOrCreate({
+          where: {
+            facebook_id: user.id
+          }
+        })
+        .then(doc => {
+          db.user
+            .update(
+              {
+                full_name: user.displayName,
+                email: "null",
+                image: user.photos[0].value
+              },
+              {
+                where: {
+                  id: doc[0].id
+                }
+              }
+            )
+            .then(() => {
+              const body = {
+                facebook_id: user.id,
+                full_name: user.displayName,
+                image: user.photos[0].value,
+                email: "null"
+              };
+              return res.json({
+                access_token: jwt.sign(body, process.env.SECRET),
+                type: "Bearer"
+              });
+            });
+        })
+        .catch(error => {
+          debug(error);
+          res.status(500).json(err);
+        });
     }
   )(req, res, next);
 });
@@ -122,28 +117,40 @@ app.get("/google/callback", (req, res, next) => {
     db.user
       .findOrCreate({
         where: {
-          google_id: user.id,
-          full_name: user.displayName,
-          // eslint-disable-next-line no-underscore-dangle
-          image: user._json.image.url,
-          email: user.emails[0].value
+          google_id: user.id
         }
       })
       .then(doc => {
-        const body = {
-          id: doc[0].id,
-          google_id: user.id,
-          full_name: user.displayName,
-          // eslint-disable-next-line no-underscore-dangle
-          image: user._json.image.url,
-          email: user.emails[0].value,
-          issued_date: Date(),
-          expired_date: Date() // add the expire date for the token from the env
-        };
-        return res.json({
-          access_token: jwt.sign(body, process.env.SECRET),
-          type: "Bearer"
-        });
+        db.user
+          .update(
+            {
+              full_name: user.displayName,
+              // eslint-disable-next-line no-underscore-dangle
+              image: user._json.image.url,
+              email: user.emails[0].value
+            },
+            {
+              where: {
+                id: doc[0].id
+              }
+            }
+          )
+          .then(() => {
+            const body = {
+              id: doc[0].id,
+              google_id: user.id,
+              full_name: user.displayName,
+              // eslint-disable-next-line no-underscore-dangle
+              image: user._json.image.url,
+              email: user.emails[0].value,
+              issued_date: Date(),
+              expired_date: Date() // add the expire date for the token from the env
+            };
+            return res.json({
+              access_token: jwt.sign(body, process.env.SECRET),
+              type: "Bearer"
+            });
+          });
       })
       .catch(error => {
         debug(error);
