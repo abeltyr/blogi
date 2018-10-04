279148d2ee157f8ab3ba5edb857172cf892e22a8
avell21 <abeltyr@gmail.com>
2018-10-04 06:29:04 +0300

    added comments

--- routes/FeedBack.js
+++ routes/FeedBack.js
@@ -6,9 +6,13 @@ const token = require("../app/middlewares/verifyToken");
 
 app
   .use((req, res, next) => token(req, res, next))
-
+  /**
+   *  this let you comment  by sending the blog_id and the comment
+   */
   .post("/New", FEED.New_comment)
-
+  /**
+   *  this let you delete the comment by sending the comment id
+   */
   .delete("/Delete/:id", FEED.Delete_comment);
 
 module.exports = app;
--- routes/blog.js
+++ routes/blog.js
@@ -7,36 +7,66 @@ const BLOG = require("../app/controllers/BlogController");
 const token = require("../app/middlewares/verifyToken");
 
 app
+  /**
+   *  send all the blogs
+   */
   .get("/all", BLOG.list_all)
-
+  /**
+   * send all the blogs of the selected category
+   */
   .get("/all/Category/:category", BLOG.list_Category)
-
+  /**
+   * send all the blogs of the selected title
+   */
   .get("/all/Title/:title", BLOG.list_Title)
-
+  /**
+   * sends the detail of the blog which blog id is
+   * send in parameters
+   */
   .get("/:id", BLOG.blog_detail)
-
+  /**
+   * send all the blogs of the selected user
+   */
   .get("/User/:user", BLOG.blog_User)
 
   // apply middleware for protection
 
   .use((req, res, next) => token(req, res, next))
-
+  /**
+   * if authenticated the user can add a new blog by the route below
+   */
   .post("/New", BLOG.New_blog)
-
+  /**
+   * if authenticated the user can update his own blog by the route below
+   */
   .put("/Update/:id", BLOG.Update_blog)
-
+  /**
+   * if authenticated the user can delete his own blog by the route below
+   */
   .delete("/Delete/:id", BLOG.Delete_blog)
-
+  /**
+   * if authenticated add blog to Favorite
+   */
   .post("/Favorite", BLOG.Add_favorite)
-
+  /**
+   * if authenticated get all blog that are in his/her Favorite
+   */
   .get("/get/Favorite", BLOG.get_favorite)
-
+  /**
+   * if authenticated delete the blog from his/her Favorite
+   */
   .delete("/Delete/Favorite/:id", BLOG.Delete_favorite)
-
+  /**
+   * if authenticated add blog to readLater
+   */
   .post("/readLater", BLOG.Add_bookmark)
-
+  /**
+   * if authenticated get all blog that are in his/her readLater
+   */
   .get("/get/readLater", BLOG.get_bookmark)
-
+  /**
+   * if authenticated delete the blog from his/her readLater
+   */
   .delete("/Delete/readLater/:id", BLOG.Delete_bookmark);
 
 module.exports = app;
